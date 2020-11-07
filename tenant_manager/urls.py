79c2220ddbc6d1a04844a095c2c2a035a8b5b7b4
gourav-shinde <gauravsanjayshinde@gmail.com>
2020-11-07 20:45:56 +0530

    update static

--- tenant_manager/urls.py
+++ tenant_manager/urls.py
@@ -14,7 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include,url
+from django.urls import path,include
+from django.conf import urls
+from django.views.static import serve
 from appcore.views import landing
 from django.conf.urls.static import static
 from django.conf import settings
